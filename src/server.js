b214d77eed481bcc6c17234bc825859d2fdff083
chonlatit <chonlatit@thinknet.co.th>
2018-05-04 13:35:53 +0700

    add cronjob

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+import cronJob from 'cron'
 
 import route from './route'
 
 const server = express()
+
+const CronJob = cronJob.CronJob
 
 const { NODE_PORT, MONGO_CONNECTION } = process.env
 
@@ -14,6 +17,11 @@ server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({
   extended: true,
 }))
+
+
+new CronJob('0 */1 * * * *', () => {
+  console.log('cron job')
+}, null, true, 'Africa/Lagos')
 
 mongoose.Promise = global.Promise
 mongoose.connect(MONGO_CONNECTION)
