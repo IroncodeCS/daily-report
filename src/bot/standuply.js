709fba197ec7aa909a7243e1718af8905f8041fc
narongchai <>
2018-05-04 13:43:24 +0700

    add first_coming_message

--- src/bot/standuply.js
+++ src/bot/standuply.js
@@ -5,13 +5,32 @@ controller
   .spawn({
     token: 'xoxb-358220335298-yAFWYRh4DsGJIoo7w5MiLhDi'
   })
-  .startRTM(function (err) {
+  .startRTM((err, bot) => {
     if (err) {
       throw new Error(err)
     }
+    bot.reply({
+      type: 'message',
+      user: 'U9B5AAHL5',
+      channel: 'DAHFDJK7A'}, 'hello')
   })
 
 let user = [{ userID: 'U9B5AAHL5', message: [], countReply: 1 }]
+
+controller.on('rtm_open', (bot, message) => {
+  // console.log('eieiei');
+  // bot.reply({
+  //   type: 'message',
+  //   user: 'U9B5AAHL5',
+  //   channel: 'DAHFDJK7A'}, 'hello')
+  // // bot.reply(message, 'test')
+  bot.api.chat.postMessage({channel: 'DAHFDJK7A', as_user: true, reply_broadcast: true, text: '666'}, (err, res) => {
+    console.log(res);
+    
+  })
+})
+
+
 controller.hears(
   [('.*')], ['direct_message', 'direct_mention', 'mention'],
   (bot, message) => {
@@ -20,12 +39,12 @@ controller.hears(
       switch (user[0].countReply) {
         case 1:
           bot.reply(message, 'วันนี้จะทำอะไรบ้าง?')
-          count++
+          user[0].countReply++
           break;
 
         case 2:
-          bot.reply(message, 'งานที่ทำอยู่ติดปัญหาอะไรบ้าง?' )
-          count++
+          bot.reply(message, 'งานที่ทำอยู่ติดปัญหาอะไรบ้าง?')
+          user[0].countReply++
           break;
 
         default:
