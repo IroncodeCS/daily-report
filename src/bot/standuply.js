cce3d189a126f9f85a67851bb96bad76e811023c
narongchai <>
2018-05-04 11:01:27 +0700

    config case

--- src/bot/standuply.js
+++ src/bot/standuply.js
@@ -11,22 +11,27 @@ controller
     }
   })
 
-let user = [{ userID: 'U9B5AAHL5', message: [], countReply: 0 }]
+let user = [{ userID: 'U9B5AAHL5', message: [], countReply: 1 }]
 controller.hears(
   [('.*')], ['direct_message', 'direct_mention', 'mention'],
   (bot, message) => {
     console.log(message);
     if (message.user === user[0].userID) {
       switch (user[0].countReply) {
-        case value:
-          
+        case 1:
+          bot.reply(message, 'วันนี้จะทำอะไรบ้าง?')
+          count++
           break;
-      
+
+        case 2:
+          bot.reply(message, 'งานที่ทำอยู่ติดปัญหาอะไรบ้าง?' )
+          count++
+          break;
+
         default:
           break;
       }
-      bot.reply(message, '' + user[0].countReply)
-      count++
+
     }
   }
 )
